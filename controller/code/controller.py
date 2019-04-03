79502c01e04434dbee79319001524a2538cca492
Simon Weideskog <Simon@n160-p133.eduroam.kth.se>
2019-04-03 10:09:32 +0200

    Mainfunktion i controller

--- controller/code/controller.py
+++ controller/code/controller.py
@@ -2,14 +2,9 @@
 code for control unit
 runs on raspberry pi zero w
 """
-<<<<<<< HEAD
 URL = "Wubbadubba.se"
 
 def main():
     
 
 main()
-=======
-print("Hello world!")
-print("Tore testar")
->>>>>>> 0a4dfacf9539bd5ff731f0f4d8cc05ebd30fab46
