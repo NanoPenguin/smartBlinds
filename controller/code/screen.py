d5e4dc81fc47ed592f9199d47160519fd13e2c71
NanoPenguin <linback98@gmail.com>
2019-04-25 16:09:37 +0200

    bugfix

--- controller/code/screen.py
+++ controller/code/screen.py
@@ -35,7 +35,7 @@ class Screen():
             scrollDirUp = False
         else:
             scrollDirUp = True
-        for animationConst in range(blockSize+1,0):
+        for animationConst in range(blockSize+1,0,-1):
             if scrollDirUp:
                 animationConst = -animationConst
             with canvas(SCREENDEVICE) as draw:
