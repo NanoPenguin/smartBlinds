f3df723a2fda3fad1891f92237bbace5ff3f53bb
Simon Weideskog <simonweideskog@live.se>
2019-04-25 13:05:15 +0200

    blinds.ino updated, calendar class added

--- blinds/code/blinds.ino
+++ blinds/code/blinds.ino
@@ -2,3 +2,17 @@
 code for blinds unit
 runs on arduino nano
 */
+const int anglePot = A1;
+int targetValue;
+const int motorPlus = 2;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
