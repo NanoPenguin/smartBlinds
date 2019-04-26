96070eedffaf7b2de1ae5d44060f67cf1bfdbfa5
NanoPenguin <linback98@gmail.com>
2019-04-26 12:02:47 +0200

    bugfix

--- controller/code/screen.py
+++ controller/code/screen.py
@@ -35,7 +35,7 @@ class Screen():
             scrollDirUp = False
         else:
             scrollDirUp = True
-        animationRange = list(range(blockSize,0,-1))
+        animationRange = [for i in range(blockSize,0,-1)]
         if scrollDirUp:
             animationRange = animationRange.reverse()
         for animationConst in animationRange:
