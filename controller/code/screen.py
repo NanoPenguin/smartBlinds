1acb9c46aa79c65c5139983eb3c5672ca81c722c
NanoPenguin <linback98@gmail.com>
2019-04-26 18:33:26 +0200

    designfix

--- controller/code/screen.py
+++ controller/code/screen.py
@@ -67,7 +67,7 @@ class Screen():
                         if alarmAuto:
                                 autoStr = "cal"
                         else:
-                                autoStr = "man"
+                                autoStr = ""
                     Y = (alarms.index(alarm)-self._currentScroll) * blockSize + animationConst - 1
                     if self._currentScroll == self._scrollIndex:
                             Y += blockSize*2
@@ -77,11 +77,11 @@ class Screen():
                     draw.line((0, Y, W, Y), fill="white")
                     draw.line((0, Y+blockSize, W, Y+blockSize), fill="white")
                     draw.rectangle((0, blockSize, 2, blockSize*2-1), fill="white")
-                    draw.ellipse((W-blockSize+9, Y+5, W-1, Y+blockSize-5), outline="white", fill=activeColor)
                     if alarm != newAlarm:
                         draw.text((4, Y+(blockSize-self._fontSize)/2), alarmTime, fill="white", font=self._fontBold)
                         autoStrSize = draw.textsize(autoStr, font=self._font)
                         draw.text((W-blockSize-autoStrSize[0], Y+(blockSize-self._fontSize)/2), autoStr, fill="white", font=self._font)
+                        draw.ellipse((W-blockSize+9, Y+5, W-1, Y+blockSize-5), outline="white", fill=activeColor)
                     else:
                         draw.text((4, Y+(blockSize-self._fontSize)/2), newAlarm, fill="white", font=self._font)
                 if self._currentScroll == self._scrollIndex:
