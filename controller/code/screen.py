cedb6fc677db1cad7b88a54c6d294f6ff216f03e
NanoPenguin <linback98@gmail.com>
2019-04-26 11:50:55 +0200

    bugfix

--- controller/code/screen.py
+++ controller/code/screen.py
@@ -53,7 +53,7 @@ class Screen():
                     else:
                             autoStr = "man"
                     Y = (alarms.index(alarm)+1-self._currentScroll) * blockSize + animationConst
-                    print('index: {}  scroll: {}  animationConst: {}  Y: {}'.format(alarms.index(alarm), self._currentScroll, animationConst, Y)
+                    print('index: {}  scroll: {}  animationConst: {}  Y: {}'.format(alarms.index(alarm), self._currentScroll, animationConst, Y))
                     draw.line((0, Y, W, Y), fill="white")
                     draw.line((0, Y+blockSize, W, Y+blockSize), fill="white")
                     draw.text((0, Y+(blockSize-fontSize)/2), alarmTime, fill="white", font=fontBold)
