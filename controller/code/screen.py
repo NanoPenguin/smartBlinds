305fef11d04f732ead54abec44c4acb816a5aeed
NanoPenguin <linback98@gmail.com>
2019-04-26 19:41:12 +0200

    adde set minute screen

--- controller/code/screen.py
+++ controller/code/screen.py
@@ -135,6 +135,20 @@ class Screen():
             draw.text(((W/2-w)/2+W/2, (H-h)/2), timeList[1], fill="white", font=self._fontClock)
 
 
+    def setMinuteScreen(self, timeStr):
+        timeList = timeStr.split(':')
+        W = 128
+        H = 64
+        Ycal = 2
+        with canvas(SCREENDEVICE) as draw:
+            w, h = draw.textsize(':', font=self._fontClock)
+            draw.text(((W-w)/2, (H-h)/2), ':', fill="white", font=self._fontClock)
+            w, h = draw.textsize(timeList[0], font=self._fontClock)
+            draw.rectangle(((W/2-w)/2+W/2, (H-h)/2+Ycal, (W/2-w)/2+w+W/2, (H-h)/2+h+Ycal), fill="white")
+            draw.text(((W/2-w)/2, (H-h)/2), timeList[0], fill="white", font=self._fontClock)
+            w, h = draw.textsize(timeList[1], font=self._fontClock)
+            draw.text(((W/2-w)/2+W/2, (H-h)/2), timeList[1], fill="black", font=self._fontClock)
+
 
     # settingsScreen handels the graphichs of the settings screen. Similar to alarmScreen.
     def settingsScreen(self):
