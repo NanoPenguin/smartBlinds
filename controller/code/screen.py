e3a4cc1bf546f574f7d44adfa99b9326ef095a9b
NanoPenguin <linback98@gmail.com>
2019-04-26 13:41:13 +0200

    testing

--- controller/code/screen.py
+++ controller/code/screen.py
@@ -16,7 +16,7 @@ class Screen():
     def __init__(self):
         self._scrollIndex = 0
         self._currentScroll = 0
-        self._scrollDelay = 0.007
+        self._scrollDelay = 0.002
 
     def clockScreen(self):
         pass
