98c8f05c7a42aa1e6dbda8d2794c7954a374d4e5
NanoPenguin <linback98@gmail.com>
2019-05-06 15:27:00 +0200

    mapping

--- controller/code/gpio.py
+++ controller/code/gpio.py
@@ -7,10 +7,10 @@ from time import sleep
 
 class Io():
     def __init__(self):  # initialize GPIO
-        self._up = Button(17)
-        self._down = Button(23)
-        self._left = Button(22)
-        self._right = Button(27)
+        self._up = Button(22)
+        self._down = Button(27)
+        self._left = Button(23)
+        self._right = Button(17)
 
 
     def waitForInput(self):
