e288c4dca16bf7154ae04ebbdfe78660a6f37dc2
NanoPenguin <linback98@gmail.com>
2019-05-06 15:20:28 +0200

    bugfix

--- controller/code/gpio.py
+++ controller/code/gpio.py
@@ -8,19 +8,19 @@ from time import sleep
 class Io():
     def __init__(self):  # initialize GPIO
         self._up = Button(17)
-        self._down = Button(27)
+        self._down = Button(23)
         self._left = Button(22)
-        self._right = Button(23)
+        self._right = Button(27)
 
 
     def waitForInput(self):
         while True:
-            if self._up.is_pressed:
+            if not self._up.is_pressed:
                 return 'up'
-            elif self._down.is_pressed:
+            elif not self._down.is_pressed:
                 return 'down'
-            elif self._left.is_pressed:
+            elif not self._left.is_pressed:
                 return 'left'
-            elif self._right.is_pressed:
+            elif not self._right.is_pressed:
                 return 'right'
             sleep(0.1)
