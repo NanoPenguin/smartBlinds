38bc2a6abfeae84bc27027246cd9cb9e731177b3
Simon Weideskog <simonweideskog@live.se>
2019-05-07 12:24:04 +0200

    sound update

--- controller/code/sound.py
+++ controller/code/sound.py
@@ -5,19 +5,19 @@ class Sound():
     def __init__(self):
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(13, GPIO.OUT)
-        self._freq = 800
+        self._freq = 500
         self._pwmPin = GPIO.PWM(13, self._freq)
 
     def makeSound(self):
         self._pwmPin.start(0)
-        self._pwmPin.ChangeDutyCycle(50)
-        freqTemp = self._freq
-        for i in range(5):
+        self._pwmPin.ChangeDutyCycle(100)
+        for i in range(10):
             sleep(0.4)
-            self._freq = (self._freq+(100))
-            self.updateFreq()
-        self._freq = freqTemp
-        self.updateFreq()
+            if i%2==0:
+                self._pwmPin.ChangeDutyCycle(0)
+            else:
+                self._pwmPin.ChangeDutyCycle(100)
+        self.stopSound()
 
     def getFreq(self): # get frequency
         return self._freq
@@ -29,12 +29,26 @@ class Sound():
         GPIO.cleanup()
 
     def increaseFreq(self):
+        self._pwmPin.start(0)
+        self._pwmPin.ChangeDutyCycle(100)
+        for i in range(6):
+            sleep(0.8)
+            if i%2==0:
+                self._pwmPin.ChangeDutyCycle(0)
+            else:
+                self._pwmPin.ChangeDutyCycle(100)
+        self.stopSound()
+"""
+    def increaseFreq(self):
         self._freq += 100
         self.updateFreq()
+"""
 
     def decreaseFreq(self):
-        self._freq -= 100
-        self.updateFreq()
+        self._pwmPin.start(0)
+        self._pwmPin.ChangeDutyCycle(100)
+        sleep(0.1)
+        self.stopSound()
 
     def updateFreq(self):
         self._pwmPin.ChangeFrequency(self._freq)
