351c6e8c29c6ae0e857093a5daeb22f0c6b373ef
torejohansson97 <49195637+torejohansson97@users.noreply.github.com>
2019-05-07 14:38:25 +0200

    Update blinds.py
    Added functionalaty to communicate with blinds

--- controller/code/blinds.py
+++ controller/code/blinds.py
@@ -1,30 +1,32 @@
 """
 Class for the state of the blinds
 """
+from communication import *
 
 class Blinds():
-    def __init__(self, angle):  # initialize blinds object
+    def __init__(self, angle = 600):  # initialize blinds object
         self._angle = angle
-        self._upAngle = 300
-        self._openAngle = 150
-        self._downAngle = 0
+        self._downAngle = 1023
+        self._openAngle = 600
+        self._upAngle = 0
+        self._blinds = Communication()
 
-
-    def getAngle(self):  # return current angle
-        return self._angle
-
+    def getBlindsData(self):  # return current angle
+        setpoint, position = self._blinds.getBlindsData()
+        
+        return setpoint, position
 
     def setAngle(self, angle):  # set new angle
         self._angle = angle
+        self._blinds.setBlinds(angle)
 
-
-    def Up(self):  # set blinds to up
+    def up(self):  # set blinds to up
         self.setAngle(self._upAngle)
 
 
-    def Down(self):  # set blinds to down
+    def down(self):  # set blinds to down
         self.setAngle(self._downAngle)
 
 
-    def Open(self):  # set blinds to open
+    def open(self):  # set blinds to open
         self.setAngle(self._openAngle)
