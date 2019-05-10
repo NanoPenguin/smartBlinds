2fb448d6f86682d35e80268feac0573d53662c7d
Simon Weideskog <simonweideskog@live.se>
2019-05-10 11:23:07 +0200

    bugfix

--- controller/code/main.py
+++ controller/code/main.py
@@ -309,7 +309,7 @@ def updateCalAlarms():
         for alarm in ALARMS:
             if alarm.isFromCalendar():
                 removeAlarm(alarm)
-                print('removed '+alarm)
+                print('removed '+str(alarm))
         for alarm in newCalAlarms:
             ALARMS.append(alarm)
 
