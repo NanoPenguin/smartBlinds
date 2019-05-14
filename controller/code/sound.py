b1e13d48f706ef5a1c817aac3b87ab52808bb8a2
NanoPenguin <linback98@gmail.com>
2019-05-14 17:48:04 +0200

    sound update

--- controller/code/sound.py
+++ controller/code/sound.py
@@ -14,12 +14,15 @@ class Sound():
         millis = int(round(time.time() * 1000))
         print(str(millis-self._lastSwitch)+' sinceLast')
         print(str(beepLength*1000) +' beeplength')
-        if(millis-self._lastSwitch>beepLength*1000):
-            if self._soundOn:
+        if self._soundOn:
+            self.IO.digitalWrite(SPEAKER_PIN,'HIGH')
+            if(millis-self._lastSwitch>beepLength*1000):
                 self.stop()
                 self._soundOn = False
                 self._lastSwitch = millis
-            else:
+        else:
+            self.stop()
+            if(millis-self._lastSwitch>beepLength*1000):
                 self.IO.digitalWrite(SPEAKER_PIN,'HIGH')
                 self._soundOn = True
                 self._lastSwitch = millis
