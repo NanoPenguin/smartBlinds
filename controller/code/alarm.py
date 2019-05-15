01e051e0114a40130b04f55c17c7bf14388bc976
Tore <torejohansson1997@gmail.com>
2019-05-15 19:31:20 +0200

    fix

--- controller/code/alarm.py
+++ controller/code/alarm.py
@@ -16,12 +16,6 @@ class Alarm():
     def __str__(self):
         return time.strftime("%H:%M", time.localtime(self._time))
 
-    def __gt__(self, other):
-        int(time.strftime("%H", time.localtime(self._time)))> int(time.strftime("%H", time.localtime(other._time)))
-
-    def __lt__(self, other):
-        int(time.strftime("%H", time.localtime(self._time)))< int(time.strftime("%H", time.localtime(other._time)))
-
     # get the set time
     def getTime(self):
         return self._time
