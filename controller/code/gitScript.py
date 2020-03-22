b5fd8ee585a1f20f8e0b836ca9b734883227c168
Lastbil <49197134+Lastbil@users.noreply.github.com>
2020-03-22 21:36:35 +0100

    Delete gitScript.py

--- controller/code/gitScript.py
+++
@@ -1,8 +0,0 @@
-import sys, os, time
-from smartBlinds.controller.code.screen import *
-SCREEN = Screen(False, False)
-SCREEN.startScreen(30)
-#time.sleep(45)
-#os.system('git clone https://github.com/NanoPenguin/smartBlinds.git')
-os.system('git -C smartBlinds pull')
-os.system('python3 ~/smartBlinds/controller/code/main.py & python3 ~/smartBlinds/controller/code/webhooklisten.py')
